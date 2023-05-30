16dcea327c188c67ab6b2cb72661e90028043812
Marie <muramatsumarie@gmail.com>
2023-05-30 16:43:44 +0900

    link project to repo

---
+++ SeedsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeedsApp
+//
+//  Created by Marie Muramatsu on 27/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
